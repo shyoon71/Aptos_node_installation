edf94c93798f4944cbf8f34b15494352cd28e9a1
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-01 17:22:22 +0900

    Update kaptos_alan_yoon_v1.sh

--- kaptos_alan_yoon_v1.sh
+++ kaptos_alan_yoon_v1.sh
@@ -124,25 +124,21 @@ else
     echo ""
     echo ""
 fi
-sleep 10
+sleep 3
 curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type &&
 echo ""
 echo "\e[1m\e[35mAlan: Your node is syncing Now, so be patient for a while. \e[0m"
+sleep 5
 echo ""
-sleep 10
 echo ""
-echo "\e[1m\e[33mAlan: If you confirm your synced number ss increasing continuously, you can ctrl+c, exit this script. \e[0m"
-sleep 10
+echo "\e[1m\e[33mAlan: If you confirm your synced number is increasing continuously, you can ctrl+c, exit this script. \e[0m"
+sleep 5
 echo ""
 echo ""
 curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type &&
-sleep 5
 echo ""
 echo ""
 echo "\e[1m\e[33mAlan: Have a nide day! \e[0m"
 echo ""
 echo ""
 echo ""
-echo ""
-echo ""
-
