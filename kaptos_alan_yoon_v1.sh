03cefc49d65864a6146e34e8f00edc1e4a9b866f
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-04 08:46:49 +0900

    Update kaptos_alan_yoon_v1.sh

--- kaptos_alan_yoon_v1.sh
+++ kaptos_alan_yoon_v1.sh
@@ -91,7 +91,7 @@ else
     sed -i 's/{}//g' /root/public_full_node.yaml &&
     sleep 1
 fi
-sed -i 's/127.0.0.1/0.0.0.0/g' /root/public_full_node.yaml &&
+sed -i 's/127.0/0.0/g' /root/public_full_node.yaml &&
 sleep 3
 grep -o "state_sync" /root/public_full_node.yaml > /root/v2_or_not.txt &&
 sleep 2
