603bfbba0e0a0321e26ab745bdbef88cdf747178
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-03 20:56:29 +0900

    Update node monitoring command.sh

--- node monitoring command.sh
+++ node monitoring command.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 echo ""
-echo "노드 구동 상태 확인을 시작합니다..."
+echo "\e[1m\e[33m노드 구동 상태 확인을 시작합니다... \e[0m"
 echo ""
 cd
 sleep 3
 timeout 15 docker stats &&
 echo ""
 echo ""
-echo "CPU 부하율과 Memory 점유율 수치가 실시간으로 변하는 지 확인바랍니다."
+echo "\e[1m\e[33mCPU 부하율과 Memory 점유율 수치가 실시간으로 변하는 지 확인바랍니다. \e[0m"
 echo ""
-echo "노드 스타트 후 초기 싱크 캐치업이 끝난 안정적인 상태라면 Memory 점유율은 50% 이하여야 합니다 "
+echo "\e[1m\e[33m노드 스타트 후 싱크 캐치업이 끝난 안정적인 상태라면 CPU 부하율과 Memory 점유율은 40% 이하가 바람직합니다. \e[0m"
 sleep 5
-cd $HOME/aptos
+cd ./aptos
+sleep 1
 timeout 15 docker compose logs -f --tail 1000 | grep "Applied transaction output chunk!" | grep local_synced_version &&
 echo ""
 echo ""
