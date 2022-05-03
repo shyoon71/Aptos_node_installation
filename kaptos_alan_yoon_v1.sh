9626cae2be77e5aeac11512196058c8e8dfd3baa
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-03 09:15:25 +0900

    Update kaptos_alan_yoon_v1.sh

--- kaptos_alan_yoon_v1.sh
+++ kaptos_alan_yoon_v1.sh
@@ -147,7 +147,7 @@ echo "\e[1m\e[35mAnd from now another script for extracting your identity info a
 echo "\e[1m\e[33mThis extracting script was also made by Andrew | zValid(discord id: @drawrowfly#4024), thanks to Andrew! \e[0m"
 echo ""
 sleep 5
-wget -q -O aptos_identity.sh https://api.zvalid.com/aptos_identity.sh && chmod +x aptos_identity.sh && sudo /bin/bash aptos_identity.sh > ../my_seed_format.txt && sed "s/^M//g" ../my_seed_format.txt > /dev/null && sed -i "s/'/\"/g" ../my_seed_format.txt
+wget -q -O aptos_identity.sh https://api.zvalid.com/aptos_identity.sh && chmod +x aptos_identity.sh && sudo /bin/bash aptos_identity.sh > ../my_seed_format.txt && sed "s/^M//g" ../my_seed_format.txt > /dev/null && sed "s/'/\"/g" ../my_seed_format.txt
 echo ""
 echo ""
 sleep 1
