a7f843d811bf2d2df4d2a47f56b47bcf0d81f4e3
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-03 20:49:36 +0900

    Update node monitoring command.sh

--- node monitoring command.sh
+++ node monitoring command.sh
@@ -3,6 +3,7 @@
 echo ""
 echo "노드 구동 상태 확인을 시작합니다..."
 echo ""
+cd
 sleep 3
 timeout 15 docker stats &&
 echo ""
@@ -11,7 +12,8 @@ echo "CPU 부하율과 Memory 점유율 수치가 실시간으로 변하는 지 
 echo ""
 echo "노드 스타트 후 초기 싱크 캐치업이 끝난 안정적인 상태라면 Memory 점유율은 50% 이하여야 합니다 "
 sleep 5
-timeout 15 $HOME/aptos/docker-compose logs -f --tail 1000 | grep "Applied transaction output chunk!" | grep local_synced_version &&
+cd $HOME/aptos
+timeout 15 docker compose logs -f --tail 1000 | grep "Applied transaction output chunk!" | grep local_synced_version &&
 echo ""
 echo ""
 echo "본인 노드 로컬 싱크정보 버전이 Aptos 블록체인의 버전을 지속적으로 따라가고 있는 지 확인바랍니다."
