4b90714b7c51dd047229964d6ca15a42155cc2dd
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-06 07:05:45 +0900

    Update compile_alan_yoon.sh

--- compile_alan_yoon.sh
+++ compile_alan_yoon.sh
@@ -91,9 +91,9 @@ else
     sleep 0.1
     PRIVATE_KEY=$(cat /root/private-key.txt)
     sleep 0.1
-    sed -i 's/<PEER_ID>/$ID/g' /root/public_full_node.yaml
+    sed -i "s/<PEER_ID>/$ID/g" /root/public_full_node.yaml
     sleep 0.1
-    sed -i 's/<PRIVATE_KEY>/$PRIVATE_KEY/g' /root/public_full_node.yaml
+    sed -i "s/<PRIVATE_KEY>/$PRIVATE_KEY/g" /root/public_full_node.yaml
     sleep 0.1
 fi
 cd /root/aptos-core 2> /dev/null
