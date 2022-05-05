70620e6a166a4f9456f870afe8ad496553ca3691
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-06 01:58:41 +0900

    Update compile_alan_yoon.sh

--- compile_alan_yoon.sh
+++ compile_alan_yoon.sh
@@ -13,7 +13,7 @@ echo "==========================================="
 sleep 1
 echo ""
 echo ""
-echo "\e[1m\e[33mPreparing now... \e[0m"
+echo "\e[1m\e[33mPreparing compile environment and source code now... \e[0m"
 sleep 1
 if [ -f /root/aptos-core/public_full_node.yaml ]
 then
