7db5cd40df7b2f6d9a1e6a492638b3d63f76bc1e
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-07 03:04:19 +0900

    Update compile_alan_yoon.sh

--- compile_alan_yoon.sh
+++ compile_alan_yoon.sh
@@ -102,9 +102,9 @@ else
     echo ""
     docker run -i -t -v /root:/root aptoslab/tools:devnet /bin/bash
     cd
-    aptos-operational-tool generate-key --encoding hex --key-type x25519 --key-file /root/private-key.txt
-    aptos-operational-tool extract-peer-from-file --encoding hex --key-file /root/private-key.txt --output-file /root/peer-info.yaml
-    exit
+#   aptos-operational-tool generate-key --encoding hex --key-type x25519 --key-file /root/private-key.txt
+#   aptos-operational-tool extract-peer-from-file --encoding hex --key-file /root/private-key.txt --output-file /root/peer-info.yaml
+    sleep 2
     ID=$(sed -n 2p /root/peer-info.yaml | sed 's/\(.*\):/\1/')
     ID=${ID//$'\r'/}
     sleep 0.1
