d4ddf0e1fbf91beb86080ca443d52508b00d39c2
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-07 23:23:25 +0900

    Delete update_alan.sh

--- update_alan.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget -q -O kaptos_alan_yoon_v1.sh https://raw.githubusercontent.com/shyoon71/installation-script/main/kaptos_alan_yoon_v1.sh && chmod +x kaptos_alan_yoon_v1.sh && sudo sh ./kaptos_alan_yoon_v1.sh
-rm update_alan.sh > /dev/null &&
-echo ""
