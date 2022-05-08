885de1666f39c8b61bb17d51c89ff2f457c371cb
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-09 05:29:24 +0900

    Update command_alan_yoon.sh

--- command_alan_yoon.sh
+++ command_alan_yoon.sh
@@ -15,7 +15,7 @@ echo "\e[1m\e[39m    systemctl start aptos-fullnode \n \e[0m"
 echo "\e[1m\e[32mTo check the Aptos node Logs: \e[0m" 
 echo "\e[1m\e[39m    journalctl -u aptos-fullnode -f \n \e[0m" 
 
-echo "\e[1m\e[32mTo check the node syncd status: >> The third [synced] number is matter! \e[0m" 
+echo "\e[1m\e[32mTo check the node syncd status:  >> The third [synced] number is matter!! \e[0m" 
 echo "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n \e[0m"
 
 
@@ -36,3 +36,6 @@ echo "\e[1m\e[39m    docker compose logs -f --tail 1000 \n \e[0m"
 
 echo "\e[1m\e[32mTo check the node syncd status: >> The third [synced] number is matter! \e[0m" 
 echo "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n \e[0m"
+
+echo "\e[1m\e[32mTo check docker cpu and memory utilization status:  >> CPU % and MEM % is matter!! \e[0m" 
+echo "\e[1m\e[39m    docker stats --no-stream \n \e[0m" 
