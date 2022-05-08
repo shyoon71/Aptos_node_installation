6e949f97c36688af155e235cbe74c7064b0a2f52
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-09 05:17:53 +0900

    Update command_alan_yoon.sh

--- command_alan_yoon.sh
+++ command_alan_yoon.sh
@@ -2,18 +2,37 @@
 
 echo "=================================================="
 
-echo -e "\e[1m\e[32mBinary mode command list \e[0m"
+echo "\e[1m\e[32mBinary mode Aptos node command list \e[0m"
 
 echo "=================================================="
 
-echo -e "\e[1m\e[32mTo stop the Aptos Node: \e[0m" 
-echo -e "\e[1m\e[39m    systemctl stop aptos-fullnode \n \e[0m" 
+echo "\e[1m\e[32mTo stop the Aptos node: \e[0m" 
+echo "\e[1m\e[39m    systemctl stop aptos-fullnode \n \e[0m" 
 
-echo -e "\e[1m\e[32mTo start the Aptos Node: \e[0m" 
-echo -e "\e[1m\e[39m    systemctl start aptos-fullnode \n \e[0m" 
+echo "\e[1m\e[32mTo start the Aptos node: \e[0m" 
+echo "\e[1m\e[39m    systemctl start aptos-fullnode \n \e[0m" 
 
-echo -e "\e[1m\e[32mTo check the Aptos Node Logs: \e[0m" 
-echo -e "\e[1m\e[39m    journalctl -u aptos-fullnode -f \n \e[0m" 
+echo "\e[1m\e[32mTo check the Aptos node Logs: \e[0m" 
+echo "\e[1m\e[39m    journalctl -u aptos-fullnode -f \n \e[0m" 
 
-echo -e "\e[1m\e[32mTo check the node status: \e[0m" 
-echo -e "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n \e[0m"
+echo "\e[1m\e[32mTo check the node syncd status: >> The third [synced] number is matter! \e[0m" 
+echo "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n \e[0m"
+
+
+echo "=================================================="
+
+echo "\e[1m\e[32mDocker mode Aptos node command list \e[0m"
+
+echo "=================================================="
+
+echo "\e[1m\e[32mTo stop the Aptos node: \e[0m" 
+echo "\e[1m\e[39m    docker compose stop \n \e[0m" 
+
+echo "\e[1m\e[32mTo start the Aptos node: \e[0m" 
+echo "\e[1m\e[39m    docker compose start \n \e[0m" 
+
+echo "\e[1m\e[32mTo check the Aptos node Logs: \e[0m" 
+echo "\e[1m\e[39m    docker compose logs -f --tail 1000 \n \e[0m" 
+
+echo "\e[1m\e[32mTo check the node syncd status: >> The third [synced] number is matter! \e[0m" 
+echo "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n \e[0m"
