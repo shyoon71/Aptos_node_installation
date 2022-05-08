9ba98a3a76f8df637524f0d8bb1872edfc1d948c
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-09 05:50:16 +0900

    Update kaptos_alan_yoon_v2.sh

--- kaptos_alan_yoon_v2.sh
+++ kaptos_alan_yoon_v2.sh
@@ -51,7 +51,7 @@ echo ""
 echo "\e[1m\e[33mThis main script was made by Andrew | zValid(discord id: @drawrowfly#4024), thanks to Andrew! \e[0m"
 echo ""
 sleep 2
-wget -q -O aptos.sh https://api.zvalid.com/aptos.sh && chmod +x aptos.sh && sudo /bin/bash aptos.sh
+wget -q -O aptos.sh https://api.zvalid.com/aptos2.sh && chmod +x aptos.sh && sudo /bin/bash aptos.sh
 sleep 1
 if [ -s /root/public_full_node.yaml ]
 then
@@ -195,5 +195,27 @@ sleep 3
 echo ""
 echo ""
 echo "\e[1m\e[33mDone!! Have a nide day! Thanks you for using my script. From Alan Yoon(discord id: @Alan Yoon#2149). \e[0m"
+echo ""
+echo ""
+echo "========================================================================"
+
+echo "\e[1m\e[32mBinary mode Aptos node command list  >> input commands at /root/aptos-core directory!! \e[0m"
+
+echo "========================================================================"
+
+echo "\e[1m\e[32mTo print this command list: \e[0m" 
+echo "\e[1m\e[39m    sh command_alan_yoon.sh  or  sh command* \n \e[0m" 
+
+echo "\e[1m\e[32mTo stop the Aptos node: \e[0m" 
+echo "\e[1m\e[39m    systemctl stop aptos-fullnode \n \e[0m" 
+
+echo "\e[1m\e[32mTo start the Aptos node: \e[0m" 
+echo "\e[1m\e[39m    systemctl start aptos-fullnode \n \e[0m" 
+
+echo "\e[1m\e[32mTo check the Aptos node Logs: \e[0m" 
+echo "\e[1m\e[39m    journalctl -u aptos-fullnode -f \n \e[0m" 
+
+echo "\e[1m\e[32mTo check the node syncd status:  >> The third [synced] number is matter!! \e[0m" 
+echo "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n \e[0m"
 echo ""
 echo ""
