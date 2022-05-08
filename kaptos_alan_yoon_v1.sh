c3a3c6c1a516efc20f25d7b98ba712c260afb07e
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-09 05:32:49 +0900

    Update kaptos_alan_yoon_v1.sh

--- kaptos_alan_yoon_v1.sh
+++ kaptos_alan_yoon_v1.sh
@@ -197,3 +197,25 @@ echo ""
 echo "\e[1m\e[33mDone!! Have a nide day! Thanks you for using my script. From Alan Yoon(discord id: @Alan Yoon#2149). \e[0m"
 echo ""
 echo ""
+echo "========================================================================================="
+
+echo "\e[1m\e[32mDocker mode Aptos node command list  >> input command at /root/aptos directory!! \e[0m"
+
+echo "========================================================================================="
+
+echo "\e[1m\e[32mTo stop the Aptos node: \e[0m" 
+echo "\e[1m\e[39m    docker compose stop \n \e[0m" 
+
+echo "\e[1m\e[32mTo start the Aptos node: \e[0m" 
+echo "\e[1m\e[39m    docker compose start \n \e[0m" 
+
+echo "\e[1m\e[32mTo check the Aptos node Logs: \e[0m" 
+echo "\e[1m\e[39m    docker compose logs -f --tail 1000 \n \e[0m" 
+
+echo "\e[1m\e[32mTo check the node syncd status: >> The third [synced] number is matter! \e[0m" 
+echo "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n \e[0m"
+
+echo "\e[1m\e[32mTo check docker cpu and memory utilization status:  >> CPU % and MEM % is matter!! \e[0m" 
+echo "\e[1m\e[39m    docker stats --no-stream \n \e[0m" 
+echo ""
+echo ""
