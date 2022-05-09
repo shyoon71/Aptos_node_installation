1b7b870abf7173859959461d7680055b62dd5bea
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-09 09:39:37 +0900

    Update kaptos_alan_yoon_v2.sh

--- kaptos_alan_yoon_v2.sh
+++ kaptos_alan_yoon_v2.sh
@@ -97,14 +97,13 @@ else
     sed -i '/{}/d' /root/public_full_node.yaml &&
     sleep 1
 fi
-#grep -o "127.0.0.1" /root/public_full_node.yaml > /root/127001.txt
-#sleep 3
-#if [ -s /root/127001.txt ]
-#then
-#    sed -i '/127.0.0.1/d' /root/public_full_node.yaml &&
-#    sed -i'' -r -e '/prevent remote, incoming connections/a\    listen_address: "/ip4/0.0.0.0/tcp/6180"' /root/public_full_node.yaml &&
-#    sleep 2
-#else
+grep -o "127.0.0.1" /root/public_full_node.yaml > /root/127001.txt &&
+sleep 1
+if [ -s /root/127001.txt ]
+then
+    sed -i "s/127.0.0.1/0.0.0.0/g" /root/public_full_node.yaml &&
+    sleep 1
+fi
 echo ""
 echo ""
 sleep 3
