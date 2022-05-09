8e6843f8bbfd366ce68c0617b67096b91534eb1e
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-09 09:32:58 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ grep -o "127.0.0.1" /root/public_full_node.yaml > /root/127001.txt
 sleep 0.1
 if [ -s /root/127001.txt ]
 then
-    sed -i '/127.0.0.1/0.0.0.0/g' /root/public_full_node.yaml &&
+    sed -i "s/127.0.0.1/0.0.0.0/g" /root/public_full_node.yaml &&
     sleep 0.1
 fi
