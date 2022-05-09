d6f47974fa448b7abec18bc3adeb6e91c5da9fc3
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-09 20:45:55 +0900

    Update command_alan_yoon.sh

--- command_alan_yoon.sh
+++ command_alan_yoon.sh
@@ -2,7 +2,7 @@
 
 echo "========================================================================"
 
-echo "\e[1m\e[32mBinary mode Aptos node command list  >> input commands at /root/aptos-core directory!! \e[0m"
+echo "\e[1m\e[33mBinary mode Aptos node command list  >> input at /root/aptos directory!! \e[0m"
 
 echo "========================================================================"
 
@@ -15,7 +15,7 @@ echo "\e[1m\e[39m    systemctl stop aptos-fullnode \n \e[0m"
 echo "\e[1m\e[32mTo start the Aptos node: \e[0m" 
 echo "\e[1m\e[39m    systemctl start aptos-fullnode \n \e[0m" 
 
-echo "\e[1m\e[32mTo check the Aptos node Logs: \e[0m" 
+echo "\e[1m\e[32mTo check the Aptos node Logs:  >> To stop inut ctrl+c \e[0m" 
 echo "\e[1m\e[39m    journalctl -u aptos-fullnode -f \n \e[0m" 
 
 echo "\e[1m\e[32mTo check the node syncd status:  >> The third [synced] number is matter!! \e[0m" 
@@ -24,7 +24,7 @@ echo "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state
 
 echo "========================================================================"
 
-echo "\e[1m\e[32mDocker mode Aptos node command list  >> input commands at /root/aptos directory!! \e[0m"
+echo "\e[1m\e[33mDocker mode Aptos node command list  >> input at /root/aptos directory!! \e[0m"
 
 echo "========================================================================"
 
@@ -37,7 +37,7 @@ echo "\e[1m\e[39m    docker compose stop \n \e[0m"
 echo "\e[1m\e[32mTo start the Aptos node: \e[0m" 
 echo "\e[1m\e[39m    docker compose start \n \e[0m" 
 
-echo "\e[1m\e[32mTo check the Aptos node Logs: \e[0m" 
+echo "\e[1m\e[32mTo check the Aptos node Logs:  >> To stop inut ctrl+c \e[0m" 
 echo "\e[1m\e[39m    docker compose logs -f --tail 1000 \n \e[0m" 
 
 echo "\e[1m\e[32mTo check the node syncd status: >> The third [synced] number is matter! \e[0m" 
@@ -45,3 +45,4 @@ echo "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state
 
 echo "\e[1m\e[32mTo check docker cpu and memory utilization status:  >> CPU % and MEM % is matter!! \e[0m" 
 echo "\e[1m\e[39m    docker stats --no-stream \n \e[0m" 
+
