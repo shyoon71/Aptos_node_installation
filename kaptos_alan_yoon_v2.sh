13a0a4b731af3f4b87f51cab0c41297d12efa6d7
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-11 03:22:09 +0900

    Update kaptos_alan_yoon_v2.sh

--- kaptos_alan_yoon_v2.sh
+++ kaptos_alan_yoon_v2.sh
@@ -168,7 +168,7 @@ echo "\e[1m\e[35mYou can find your correct seed format file at /root directory, 
 sleep 5
 rm /root/default_seed.txt &> /dev/null &&
 rm /root/v2_or_not.txt &> /dev/null &&
-rm /root/aptos/aptos.sh &> /dev/null &&
+rm /root/reference_compile_mode.sh &> /dev/null &&
 rm /root/kaptos_alan_yoon_v2.sh &> /dev/null &&
 rm /root/127001.txt &> /dev/null &&
 rm /root/seed.txt &> /dev/null &&
