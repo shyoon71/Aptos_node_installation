2d566e042318436712fb9f34586ad899aa89046f
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-12 12:34:07 +0900

    Update kaptos_alan_yoon_v2.sh

--- kaptos_alan_yoon_v2.sh
+++ kaptos_alan_yoon_v2.sh
@@ -211,6 +211,7 @@ sleep 0.2
 wget -q -O disk.sh https://raw.githubusercontent.com/shyoon71/installation-script/main/disk.sh && chmod +x disk.sh && sudo /bin/bash disk.sh
 sleep 0.2
 echo ""
+echo ""
 echo "========================================================================"
 
 echo "\e[1m\e[33mBinary mode Aptos node command list  >> input at /root/aptos directory!! \e[0m"
