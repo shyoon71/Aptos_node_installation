fcbf6c7f958f8d39850a7c827e0ea071fdcb3a48
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-12 11:13:05 +0900

    Create disk_alan_yoon.sh

---
+++ disk_alan_yoon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TOTAL=$(df -P | grep -v ^Filesystem | awk '{sum += $2} END { print sum/1024/1024 " GB" }')
+USED=$(df -P | grep -v ^Filesystem | awk '{sum += $3} END { print sum/1024/1024 " GB" }')
+RATIO=`echo "100*$USED/$TOTAL" | bc -l`
+
+echo "====================================================="
+echo -en "\n"
+echo -e "DISK Total Size: \e[0m" $TOTAL
+echo -e "DISK Used  Size: \e[0m" $USED
+echo -e "DISK Used Ratio: \e[0m" $RATIO
+echo "====================================================="
+echo -en "\n"
