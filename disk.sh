4e93a035656e980aaf17651cbd1f6f8f06f14681
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-12 12:32:05 +0900

    Update disk.sh

--- disk.sh
+++ disk.sh
@@ -3,8 +3,8 @@
 TOTAL=$(df -P | grep -v ^Filesystem | awk '{sum += $2} END { print sum/1024/1024 " GB" }')
 USED=$(df -P | grep -v ^Filesystem | awk '{sum += $3} END { print sum/1024/1024 " GB" }')
 
-echo "============================="
+echo "============================"
 echo -e "DISK Total Size: \e[0m" $TOTAL
 echo -e "DISK Used  Size: \e[0m" $USED
-echo "============================="
+echo "============================"
 echo -en "\n"
