4bb5c8d4aeecd0bb1e29267acc652e25e7ef260f
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-17 21:09:06 +0900

    Update testnet_alanyoon.sh

--- testnet_alanyoon.sh
+++ testnet_alanyoon.sh
@@ -87,7 +87,9 @@ aptos key generate --output-file root-key.yaml && sleep 1
 
 ROOT=$(cat root-key.yaml) && sleep 0.2
 
-sed -i'' 's/root_key: "0x5243ca72b0766d9e9cbf2debf6153443b01a1e0e6d086c7ea206eaf6f8043956"/root_key: $ROOT/g' /root/layout.yaml && sleep 1
+sed -i '/root_key:/d' layout.yaml
+
+sed -i'' -r -e "/---/a\root_key: $ROOT/" layout.yaml && sleep 1
 
 wget https://github.com/aptos-labs/aptos-core/releases/download/aptos-framework-v0.1.0/framework.zip && sleep 0.2
 
