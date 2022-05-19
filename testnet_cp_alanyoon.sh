0a0fd1b64c99adb4d4aaa01876c4d99d743f48f9
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-19 11:35:16 +0900

    Update testnet_cp_alanyoon.sh

--- testnet_cp_alanyoon.sh
+++ testnet_cp_alanyoon.sh
@@ -15,46 +15,59 @@ cd $HOME/aptos
 rm *
 rm $HOME/aptos/identity/id.json
 cp $HOME/aptos-core/config/src/config/test_data/public_full_node.yaml $HOME/aptos
-wget -P $HOME/aptos https://devnet.aptoslabs.com/genesis.blob
-wget -P $HOME/aptos https://devnet.aptoslabs.com/waypoint.txt
-/usr/local/bin/yq e -i 'del(.base.waypoint.from_file)' $HOME/aptos/public_full_node.yaml
-/usr/local/bin/yq e -i '.base.waypoint.from_config="'$(cat $HOME/aptos/waypoint.txt)'"' $HOME/aptos/public_full_node.yaml
-/usr/local/bin/yq e -i '.execution.genesis_file_location = "'$HOME/aptos/genesis.blob'"' $HOME/aptos/public_full_node.yaml
+# wget -P $HOME/aptos https://devnet.aptoslabs.com/genesis.blob
+# wget -P $HOME/aptos https://devnet.aptoslabs.com/waypoint.txt
+# /usr/local/bin/yq e -i 'del(.base.waypoint.from_file)' $HOME/aptos/public_full_node.yaml
+# /usr/local/bin/yq e -i '.base.waypoint.from_config="'$(cat $HOME/aptos/waypoint.txt)'"' $HOME/aptos/public_full_node.yaml
+# /usr/local/bin/yq e -i '.execution.genesis_file_location = "'$HOME/aptos/genesis.blob'"' $HOME/aptos/public_full_node.yaml
 cd $HOME/aptos-core
 git checkout origin/devnet &> /dev/null
 echo y | ./scripts/dev_setup.sh
 source ~/.cargo/env
-cargo build -p aptos-node --release
-cargo build -p aptos-operational-tool --release
-mv $HOME/aptos-core/target/release/aptos-node /usr/local/bin
-mv $HOME/aptos-core/target/release/aptos-operational-tool /usr/local/bin
-if [ ! -f $HOME/aptos/identity/private-key.txt ]
-then
-    /usr/local/bin/aptos-operational-tool generate-key --encoding hex --key-type x25519 --key-file $HOME/aptos/identity/private-key.txt &> /dev/null
-fi
-/usr/local/bin/aptos-operational-tool extract-peer-from-file --encoding hex --key-file $HOME/aptos/identity/private-key.txt --output-file $HOME/aptos/identity/peer-info.yaml &> /dev/null
-PEER_ID=$(sed -n 2p $HOME/aptos/identity/peer-info.yaml | sed 's/.$//')
-PRIVATE_KEY=$(cat $HOME/aptos/identity/private-key.txt)
-/usr/local/bin/yq e -i '.full_node_networks[] +=  { "identity": {"type": "from_config", "key": "'$PRIVATE_KEY'", "peer_id": "'$PEER_ID'"} }' $HOME/aptos/public_full_node.yaml
-/usr/local/bin/yq e -i '.full_node_networks[].listen_address="/ip4/0.0.0.0/tcp/6180"' $HOME/aptos/public_full_node.yaml
+git checkout --track origin/testnet
+export WORKSPACE=testnet
+mkdir ~/$WORKSPACE
+cargo run --release -p aptos -- genesis generate-keys --output-dir ~/$WORKSPACE
+echo ""
+read -p "What's your validator IP address? : " IP
+echo ""
+read -p "What's your ID? Don't use '#' or 'space' : " ID
+echo ""
+cargo run --release -p aptos -- genesis set-validator-configuration --keys-dir ~/$WORKSPACE --local-repository-dir ~/$WORKSPACE --username $ID --validator-host $IP:6180 --full-node-host $IP:6182
+touch ~/$WORKSPACE/layout.yaml
+echo '---
+root_key: "0x5243ca72b0766d9e9cbf2debf6153443b01a1e0e6d086c7ea206eaf6f8043956"
+users:
+  - '$ID'
+chain_id: 23' > ~/$WORKSPACE/layout.yaml && sleep 0.5
+cargo run --release --package framework -- --package aptos-framework --output current
+mkdir ~/$WORKSPACE/framework
+mv aptos-framework/releases/artifacts/current/build/**/bytecode_modules/*.mv ~/$WORKSPACE/framework/
+cargo run --release -p aptos -- genesis generate-genesis --local-repository-dir ~/$WORKSPACE --output-dir ~/$WORKSPACE
+mkdir ~/$WORKSPACE/config
+cp docker/compose/aptos-node/validator.yaml ~/$WORKSPACE/validator.yaml
+cp docker/compose/aptos-node/fullnode.yaml ~/$WORKSPACE/fullnode.yaml
 
-echo "[Unit]
-Description=Aptos Node
+cargo run -p aptos-node --release -- -f ~/$WORKSPACE/validator.yaml && sleep 1
+cargo run -p aptos-node --release -- -f ~/$WORKSPACE/fullnode.yaml
 
-[Service]
-User=$USER
-Type=simple
-ExecStart=/usr/local/bin/aptos-node --config $HOME/aptos/public_full_node.yaml
-Restart=always
-RestartSec=10
-LimitNOFILE=10000
+# echo "[Unit]
+# Description=Aptos Node
 
-[Install]
-WantedBy=multi-user.target
-" > $HOME/aptos-fullnode.service
-mv $HOME/aptos-fullnode.service /etc/systemd/system
+# [Service]
+# User=$USER
+# Type=simple
+# ExecStart=/usr/local/bin/aptos-node --config $HOME/aptos/public_full_node.yaml
+# Restart=always
+# RestartSec=10
+# LimitNOFILE=10000
 
-sudo systemctl restart systemd-journald
-sudo systemctl daemon-reload
-sudo systemctl enable aptos-fullnode
-sudo systemctl restart aptos-fullnode
+# [Install]
+# WantedBy=multi-user.target
+# " > $HOME/aptos-fullnode.service
+# mv $HOME/aptos-fullnode.service /etc/systemd/system
+
+# sudo systemctl restart systemd-journald
+# sudo systemctl daemon-reload
+# sudo systemctl enable aptos-fullnode
+# sudo systemctl restart aptos-fullnode
