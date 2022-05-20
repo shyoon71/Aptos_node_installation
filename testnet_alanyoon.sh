901c2cd4907f61e5a74feec8d14390a7fd480265
Alan_Yoon <shwan.yoon@hotmail.com>
2022-05-20 23:10:27 +0900

    Update testnet_alanyoon.sh

--- testnet_alanyoon.sh
+++ testnet_alanyoon.sh
@@ -3,9 +3,6 @@ clear
 echo ""
 echo ""
 cd ~
-echo "=========================================================================="
-echo "This script is made for testnet validator node setup only. From Alan Yoon."
-echo "=========================================================================="
 echo ""
 echo ""
 read -p "What's your validator IP address? : " IP
