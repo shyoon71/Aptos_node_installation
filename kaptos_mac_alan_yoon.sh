920850b4603ed250e5b23353d0524a31c9da70a9
Alan_Yoon <shwan.yoon@hotmail.com>
2022-06-03 22:57:50 +0900

    Update kaptos_mac_alan_yoon.sh

--- kaptos_mac_alan_yoon.sh
+++ kaptos_mac_alan_yoon.sh
@@ -75,6 +75,7 @@ then
     #echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     #brew update
     #brew install docker-ce docker-ce-cli containerd.io -y
+    echo ""
 fi
 
 echo "=================================================="
