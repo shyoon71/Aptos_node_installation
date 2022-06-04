15658c5a97af076a49e14e60dba5bbfb2b74c53f
Alan_Yoon <shwan.yoon@hotmail.com>
2022-06-04 14:08:42 +0900

    Update kaptos_alan_yoon_v2.sh

--- kaptos_alan_yoon_v2.sh
+++ kaptos_alan_yoon_v2.sh
@@ -2,15 +2,18 @@
 
 clear
 cd ~
-sleep 0.1
-apt-get install figlet &> /dev/null &&
 echo ""
+echo " ============================================================================="
 echo ""
-echo "==========================================="
-figlet AlanYoon
-echo "==========================================="
-sleep 0.1
+echo "   █████╗ ██╗      █████╗ ███╗   ██╗    ██╗   ██╗ ██████╗  ██████╗ ███╗   ██╗"
+echo "  ██╔══██╗██║     ██╔══██╗████╗  ██║    ╚██╗ ██╔╝██╔═══██╗██╔═══██╗████╗  ██║"
+echo "  ███████║██║     ███████║██╔██╗ ██║     ╚████╔╝ ██║   ██║██║   ██║██╔██╗ ██║"
+echo "  ██╔══██║██║     ██╔══██║██║╚██╗██║      ╚██╔╝  ██║   ██║██║   ██║██║╚██╗██║"
+echo "  ██║  ██║███████╗██║  ██║██║ ╚████║       ██║   ╚██████╔╝╚██████╔╝██║ ╚████║"
+echo "  ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝       ╚═╝    ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝"
 echo ""
+echo " ============================================================================="
+sleep 2
 echo ""
 echo "\e[1m\e[33mStarting script now... \e[0m"
 sleep 3
@@ -235,22 +238,22 @@ echo "\e[1m\e[33mBinary mode Aptos node command list  >> input at /root/aptos di
 
 echo "========================================================================"
 
-echo "\e[1m\e[32mTo print this command list: \e[0m" 
-echo "\e[1m\e[39m    sh command_alan_yoon.sh  or  sh command* \n \e[0m" 
+echo "\e[1m\e[32mTo print this command list: \e[0m"
+echo "\e[1m\e[39m    sh command_alan_yoon.sh  or  sh command* \n \e[0m"
 
-echo "\e[1m\e[32mTo stop the Aptos node: \e[0m" 
-echo "\e[1m\e[39m    systemctl stop aptos-fullnode \n \e[0m" 
+echo "\e[1m\e[32mTo stop the Aptos node: \e[0m"
+echo "\e[1m\e[39m    systemctl stop aptos-fullnode \n \e[0m"
 
-echo "\e[1m\e[32mTo start the Aptos node: \e[0m" 
-echo "\e[1m\e[39m    systemctl start aptos-fullnode \n \e[0m" 
+echo "\e[1m\e[32mTo start the Aptos node: \e[0m"
+echo "\e[1m\e[39m    systemctl start aptos-fullnode \n \e[0m"
 
-echo "\e[1m\e[32mTo check the Aptos node Logs:  >> To stop inut ctrl+c \e[0m" 
-echo "\e[1m\e[39m    journalctl -u aptos-fullnode -f \n \e[0m" 
+echo "\e[1m\e[32mTo check the Aptos node Logs:  >> To stop inut ctrl+c \e[0m"
+echo "\e[1m\e[39m    journalctl -u aptos-fullnode -f \n \e[0m"
 
-echo "\e[1m\e[32mTo check outbound connection status: >> If number is not 0, it's ok! \e[0m" 
+echo "\e[1m\e[32mTo check outbound connection status: >> If number is not 0, it's ok! \e[0m"
 echo "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_connections{direction=\"outbound\"' \e[0m"
 
-echo "\e[1m\e[32mTo check syncd status:  >> The third [synced] number is matter!! \e[0m" 
+echo "\e[1m\e[32mTo check syncd status:  >> The third [synced] number is matter!! \e[0m"
 echo "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n \e[0m"
 echo ""
 echo ""
