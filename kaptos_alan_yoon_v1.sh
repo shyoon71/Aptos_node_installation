60e727087510f02c090af5cbf0686238ec8d46a2
Alan_Yoon <shwan.yoon@hotmail.com>
2022-06-10 20:37:05 +0900

    Update kaptos_alan_yoon_v1.sh

--- kaptos_alan_yoon_v1.sh
+++ kaptos_alan_yoon_v1.sh
@@ -210,7 +210,7 @@ rm /root/kaptos_alan_yoon_v1.sh &> /dev/null &&
 rm /root/127001.txt &> /dev/null &&
 rm /root/seed.txt &> /dev/null &&
 rm /root/6180.txt &> /dev/null &&
-rm /root/admin_seed.txt &> /dev/null &&
+#rm /root/admin_seed.txt &> /dev/null &&
 sleep 5
 echo ""
 echo ""
