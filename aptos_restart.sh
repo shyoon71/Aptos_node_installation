73efbcc4c5684a2ff1cf2aa2a3c645c4cfdbaf79
Alan_Yoon <shwan.yoon@hotmail.com>
2022-06-13 05:14:12 +0900

    Update aptos_restart.sh

--- aptos_restart.sh
+++ aptos_restart.sh
@@ -7,11 +7,14 @@ do
     countb=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_timeout_total')
     count=$((counta + countb))
     ref=20
-    count1=$(echo $count | grep -o '[0-9]*')
+    count1a=$(echo $counta | grep -o '[0-9]*')
+    count1b=$(echo $countb | grep -o '[0-9]*')
     sleep 600
-    count=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_storage_synchronizer_errors{error_label="unexpected_error"')
-    count2=$(echo $count | grep -o '[0-9]*')
-    count3=$((count2 - count1))
+    counta=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_continuous_syncer_errors{error_label="unexpected_error"}')
+    countb=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_timeout_total')
+    count2a=$(echo $counta | grep -o '[0-9]*')
+    count2b=$(echo $countb | grep -o '[0-9]*')
+    count3=$((count2a + count2b - count1a - count1b))
     if [ $count3 -gt $ref ]
     then
         count4=$((count3 / ref))
