615f8a39d25c73cb527d0da8e72ec9f5e048de71
Alan_Yoon <shwan.yoon@hotmail.com>
2022-06-13 03:45:36 +0900

    Update aptos_restart.sh

--- aptos_restart.sh
+++ aptos_restart.sh
@@ -4,7 +4,7 @@ while true
 do
     cd $HOME/aptos
     count=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_storage_synchronizer_errors{error_label="unexpected_error"')
-    ref=10
+    ref=20
     count1=$(echo $count | grep -o '[0-9]*')
     sleep 600
     count=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_storage_synchronizer_errors{error_label="unexpected_error"')
@@ -16,7 +16,7 @@ do
         echo "\e[1m\e[33mSynchronizer_error is occurring, your node shoud be restarted now... \e[0m"
         echo "\e[1m\e[33mError count per minute : \e[1m\e[35m"$count4" \e[0m"
         echo ""
-        #docker compose down && docker compose up -d
+        docker compose down && docker compose up -d
         echo ""
     else
         echo "\e[1m\e[33mYour node health is ok. \e[0m"
