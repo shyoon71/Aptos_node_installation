7b7ecff9c7f1dcdff164a9a1f40d04eeee1ee8b4
Alan_Yoon <shwan.yoon@hotmail.com>
2022-06-13 03:18:06 +0900

    Update aptos_restart.sh

--- aptos_restart.sh
+++ aptos_restart.sh
@@ -3,17 +3,18 @@ count=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_storag
 while true
 do
     cd $HOME/aptos
+    ref=10
     count1=$(echo $count | grep -o '[0-9]*')
-    sleep 30
+    sleep 600
     count2=$(echo $count | grep -o '[0-9]*')
-    count3=$(expr $count2 - $count1)
-    if [ int($count3) -gt 10 ]
+    count3=$((count2 - count1))
+    if [ $count3 -gt $ref ]
     then
-        count4=$(expr $count3 / 10)
-        echo "\e[1m\e[33mSynchronizer_error is occurring, so your node shoud be restarted now... \e[0m"
+        count4=$((count3 / ref))
+        echo "\e[1m\e[33mSynchronizer_error is occurring, your node shoud be restarted now... \e[0m"
         echo "\e[1m\e[33mError count per minute : \e[1m\e[35m"$count4" \e[0m"
         echo ""
-        docker compose down && docker compose up -d
+        #docker compose down && docker compose up -d
         echo ""
     else
         echo "\e[1m\e[33mYour node health is ok. \e[0m"
