95c521c6dd6e69c63f39118faa820ce7a69e4171
Alan_Yoon <shwan.yoon@hotmail.com>
2022-06-13 01:28:39 +0900

    Update aptos_restart.sh

--- aptos_restart.sh
+++ aptos_restart.sh
@@ -4,13 +4,14 @@ while true
 do
     cd $HOME/aptos
     count1=$(echo $count | grep -o '[0-9]*')
-    sleep 60
+    sleep 600
     count2=$(echo $count | grep -o '[0-9]*')
     count3=$(expr $count2 - $count1)
-    if [ $count3 -gt 0 ]
+    if [ $count3 -gt 10 ]
     then
+        count4=$(expr $count3 / 10)
         echo "\e[1m\e[33mSynchronizer_error is occurring, so your node shoud be restarted now... \e[0m"
-        echo "\e[1m\e[33m"$count3"times occured in 1 minute. \e[0m"
+        echo "\e[1m\e[33mError count per minute : \e[1m\e[33m"$count4" \e[0m"
         docker compose down && docker compose up -d
     fi
 done
