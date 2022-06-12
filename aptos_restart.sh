ddc5e6f8c8ba1c60641f1f335da512c96428707d
Alan_Yoon <shwan.yoon@hotmail.com>
2022-06-13 01:05:39 +0900

    Update aptos_restart.sh

--- aptos_restart.sh
+++ aptos_restart.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
+count=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_storage_synchronizer_errors{error_label="unexpected_error"')
 while true
 do
     cd $HOME/aptos
-    count=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_storage_synchronizer_errors{error_label="unexpected_error"')
     count1=$(echo $count | grep -o '[0-9]*')
     sleep 60
     count2=$(echo $count | grep -o '[0-9]*')
-    count3=$($count2-$count1)
+    count3=$(expr $count2 - $count1)
     if [ $count3 > 0 ]
     then
         echo "\e[1m\e[33mSynchronizer_error is occurring, so your node shoud be restarted now... \e[0m"
