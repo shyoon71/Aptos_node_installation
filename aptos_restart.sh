11fbf7d70dfd2d7b8d360037fcc1d0f5dafbbf60
Alan_Yoon <shwan.yoon@hotmail.com>
2022-06-13 01:11:25 +0900

    Update aptos_restart.sh

--- aptos_restart.sh
+++ aptos_restart.sh
@@ -7,7 +7,7 @@ do
     sleep 60
     count2=$(echo $count | grep -o '[0-9]*')
     count3=$(expr $count2 - $count1)
-    if [ $count3 > 0 ]
+    if [ $count3 -gt 0 ]
     then
         echo "\e[1m\e[33mSynchronizer_error is occurring, so your node shoud be restarted now... \e[0m"
         echo "\e[1m\e[33m"$count3"times occured in 1 minute. \e[0m"
