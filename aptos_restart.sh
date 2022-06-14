48a30bc401be97c86ceec45c48e165c42ebd96b6
Alan_Yoon <shwan.yoon@hotmail.com>
2022-06-15 02:11:20 +0900

    Update aptos_restart.sh

--- aptos_restart.sh
+++ aptos_restart.sh
@@ -5,7 +5,7 @@ while [ $A -lt 1008 ]
 do
     counta=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_continuous_syncer_errors{error_label="unexpected_error"}')
     countb=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_timeout_total')
-    ref=20
+    ref=100
     count1a=$(echo $counta | grep -o '[0-9]*')
     count1b=$(echo $countb | grep -o '[0-9]*')
     sleep 600
@@ -17,16 +17,17 @@ do
     count4=$(echo "scale=1;$count3 / 10" | bc -l)
     if [ $count3 -gt $ref ]
     then
-        echo "Node restarts now!! Sync error occurred count per minute : "$count4""
         today=$(date)
-        echo $today
-        docker compose down &> /dev/null && docker compose up -d &> /dev/null
+        echo " "$today"  Node Restarts Now!! Sync error occurred count per minute : "$count4""
+        docker compose restart
         echo ""
     else
-        echo "Node health is ok. Sync error occurred count per minute : "$count4""
-        today=$(date)
-        echo $today
-        echo ""
+        if [ $count3 -ne 0 ]
+        then
+            today=$(date)
+            echo " "$today"  Node health is ok. Sync error occurred count per minute : "$count4""
+            echo ""
+        fi
     fi
     A=`expr $A + 1`
 done
