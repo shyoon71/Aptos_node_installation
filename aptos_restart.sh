5b4a884c47545c61adf8a0085d4f0da56c05f68e
Alan_Yoon <shwan.yoon@hotmail.com>
2022-06-17 08:01:17 +0900

    Update aptos_restart.sh

--- aptos_restart.sh
+++ aptos_restart.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 A=1
-while [ $A -lt 576 ]
+while [ $A -lt 1441 ]
 do
     counta=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_continuous_syncer_errors{error_label="unexpected_error"}')
     countb=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_timeout_total')
-    countc=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_storage_ledger_version')
-    ref=500
+    countbb=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_continuous_syncer_errors{error_label="data_stream_notification_timeout"}')
+    countc=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_version{type="synced"}')
+    ref=60
     count1a=$(echo $counta | grep -o '[0-9]*')
     count1b=$(echo $countb | grep -o '[0-9]*')
+    count1bb=$(echo $countbb | grep -o '[0-9]*')
     count1c=$(echo $countc | grep -o '[0-9]*')
-    sleep 300
+    sleep 60
     counta=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_continuous_syncer_errors{error_label="unexpected_error"}')
     countb=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_timeout_total')
-    countc=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_storage_ledger_version')
+    countbb=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_continuous_syncer_errors{error_label="data_stream_notification_timeout"}')
+    countc=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep 'aptos_state_sync_version{type="synced"}')
     count2a=$(echo $counta | grep -o '[0-9]*')
     count2b=$(echo $countb | grep -o '[0-9]*')
+    count2bb=$(echo $countbb | grep -o '[0-9]*')
     count2c=$(echo $countc | grep -o '[0-9]*')
-    count3=$((count2a + count2b - count1a - count1b))
-    count4=$((count3 / 10))
+    count3=$((count2a + count2b + count2bb - count1a - count1b - count1bb))
+    count4=$((count3 / 1))
     count45=$(echo $count5)
     tilt=$((count45 / 5))
     count5=$((count2c - count1c))
@@ -27,10 +31,10 @@ do
         if [ $count3 -eq 0 ]
         then
             today=$(date)
-            echo " "$today"  Node looks like already stopped now."
+            echo " "$today"  Node looks like already stopped, it's not running now."
         else
             today=$(date)
-            echo " "$today"  Syncing Stopped!!! Former_version : "$count1c", Present_version : "$count2c""
+            echo " "$today"  Syncing Stopped!!! Former_synced : "$count1c", Present_synced : "$count2c""
             docker compose restart
         fi
     else
@@ -51,16 +55,16 @@ do
             if [ $count3 -eq 0 ]
             then
                 today=$(date)
-                echo " "$today"  Node looks like catchup completed now. Former_version : "$count1c", Present_version : "$count2c""
+                echo " "$today"  Node looks like catchup completed now. Former_synced : "$count1c", Present_synced : "$count2c""
             else
                 if [ $count3 -gt $ref ]
                 then
                     today=$(date)
-                    echo " "$today"  Syncing speed has fallen below 20%!! Former_version : "$count1c", Present_version : "$count2c""
+                    echo " "$today"  Syncing speed has fallen below 20%!! Former_synced : "$count1c", Present_synced : "$count2c""
                     docker compose restart
                 else
                     today=$(date)
-                    echo " "$today"  Syncing speed has fallen below 20%!! Former_version : "$count1c", Present_version : "$count2c""
+                    echo " "$today"  Syncing speed has fallen below 20%!! Former_synced : "$count1c", Present_synced : "$count2c""
                     echo " "$today"  The error count level is not so high. Sync error: "$count4""/"min"
                     echo " "$today"  No need to restart now. But node health should be checked!!"
                 fi
