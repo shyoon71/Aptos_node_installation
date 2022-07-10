71d2e65fad3253e337c6445b44d49994cdc186eb
Alan_Yoon <shwan.yoon@hotmail.com>
2022-07-10 22:11:56 +0900

    Update cli_checker_validator.sh

--- cli_checker_validator.sh
+++ cli_checker_validator.sh
@@ -4,35 +4,14 @@ echo "================================"
 echo ""
 echo "Script from  //-\ ][_ //-\ ][\][ ";
 echo ""
-echo "================================  This script is for validator only."
+echo "================================  This script is for "validators installed by docker" only."
 echo ""
 echo ""
 count=0
-highest=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "highest")
-highest=$(echo "$highest"|sed -n -e '5p')
-highest2=$(echo $highest | grep -o '[0-9]*')
-echo "================================"
-echo "$highest"
-sleep 2
-sync2=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "aptos_state_sync_version")
-sync2=$(echo "$sync2"|sed -n -e '5p')
-sync4=$(echo $sync2 | grep -o '[0-9]*')
-echo "$sync2"
-echo "================================"
-if [ $sync4 -gt $highest2 ]
-then
-    echo "ok.No lag."
-else
-    if [ $highest2 -gt $sync4 ]
-    then
-        lag=`expr $highest2 - $sync4`
-        echo 'You have to catch up with highest_version about '$lag''
-    fi
-fi
-echo ""
 sync=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "aptos_state_sync_version")
 sync=$(echo "$sync"|sed -n -e '5p')
 sync3=$(echo $sync | grep -o '[0-9]*')
+echo "Syncing Progress"
 echo "================================"
 echo "$sync"
 sleep 2
@@ -52,23 +31,46 @@ else
     echo ">>>> Not ok!! <<<<"
 fi
 echo ""
+highest=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "highest")
+highest=$(echo "$highest"|sed -n -e '5p')
+highest2=$(echo $highest | grep -o '[0-9]*')
+echo "Syncing Speed"
+echo "================================"
+echo "$highest"
 sleep 2
+sync2=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "aptos_state_sync_version")
+sync2=$(echo "$sync2"|sed -n -e '5p')
+sync4=$(echo $sync2 | grep -o '[0-9]*')
+echo "$sync2"
+echo "================================"
+if [ $sync4 -gt $highest2 ]
+then
+    echo "ok.No lag."
+else
+    if [ $highest2 -gt $sync4 ]
+    then
+        lag=`expr $highest2 - $sync4`
+        echo 'You have to catch up with highest_version about '$lag''
+    fi
+fi
+echo ""
 in=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "inbound")
 in=$(echo "$in"|sed -n -e '1p')
 in3=$(echo $in | grep -o '[0-9]*')
 export in4=$(echo "${in3:(-2)}")
+echo "Inbound Connection"
 echo "================================"
 echo "$in"
 echo "================================"
 if [ -z $in4 ]
 then
-    echo ">>>> Not ok!! <<<<"
+    echo "Can't fetch out your connection status."
 else
     if [ $in4 -eq 0 ]
     then
         echo ">>>> Not ok!! <<<< if you are already in AIT period now."
     else
-        echo "ok"
+        echo "ok."
         count=`expr $count + 1`
     fi
 fi
@@ -77,24 +79,26 @@ out=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "outbound")
 out=$(echo "$out"|sed -n -e '1p')
 out3=$(echo $out | grep -o '[0-9]*')
 export out4=$(echo "${out3:(-2)}")
+echo "Outbound Connection"
 echo "================================"
 echo "$out"
 echo "================================"
 if [ -z $out4 ]
 then
-    echo "Validator has no peers. No problem."
+    echo "Can't fetch out your connection status."
 else
     if [ $out4 -eq 0 ]
     then
-        echo "Validator has no peers. No problem."
+        echo "Validators don't need to outbound. No problem."
     else
-        echo "ok"
+        echo "ok."
     fi
 fi
 echo ""
 v1=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "aptos_consensus_vote_nil_count")
 v1=$(echo "$v1"|sed -n -e '3p')
 v3=$(echo $v1 | grep -o '[0-9]*')
+echo "Voting Progress"
 echo "================================"
 echo "$v1"
 sleep 3
@@ -103,18 +107,24 @@ v5=$(echo "$v5"|sed -n -e '3p')
 v6=$(echo $v5 | grep -o '[0-9]*')
 echo "$v5"
 echo "================================"
-if [ $v6 -gt $v3 ]
+if [ -z $v6 ]
 then
-    echo "ok."
-    count=`expr $count + 1`
+    echo "Can't fetch out your consensus status."
 else
-    echo ">>>> Not ok!! <<<<"
+    if [ $v6 -gt $v3 ]
+    then
+        echo "ok."
+        count=`expr $count + 1`
+    else
+        echo ">>>> Not ok!! <<<<"
+    fi
 fi
 echo ""
 sleep 2
 r1=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "aptos_consensus_current_round")
 r1=$(echo "$r1"|sed -n -e '3p')
 r3=$(echo $r1 | grep -o '[0-9]*')
+echo "Consensus Round Progess"
 echo "================================"
 echo "$r1"
 sleep 2
@@ -123,22 +133,33 @@ r5=$(echo "$r5"|sed -n -e '3p')
 r6=$(echo $r5 | grep -o '[0-9]*')
 echo "$r5"
 echo "================================"
-if [ $r6 -gt $r3 ]
+if [ -z $r6 ]
 then
-    echo "ok."
-    count=`expr $count + 1`
+    echo "Can't fetch out your consensus status."
 else
-    echo ">>>> Not ok!! <<<<"
+    if [ $r6 -gt $r3 ]
+    then
+        echo "ok."
+        count=`expr $count + 1`
+    else
+        echo ">>>> Not ok!! <<<<"
+    fi
 fi
 echo ""
-v7=`echo "scale=2;$v6*100/$r6"|bc`
-echo "================================"
-echo 'vote_rate_now : '$v7'%  should be >=25% at the end of the test period.'
-echo "================================"
-echo ""
-if [[ `echo "$v7 > 60" | bc` -eq 1 ]]
+if [ -z $r6 ]
 then
-    count=`expr $count + 1`
+    echo ""
+else
+    v7=`echo "scale=2;$v6*100/$r6"|bc`
+    echo "Voting Success Ratio"
+    echo "============================="
+    echo 'Ratio_now : '$v7'%  should be >=25% at the end of the test period.'
+    echo "============================="
+    echo ""
+    if [[ `echo "$v7 > 60" | bc` -eq 1 ]]
+    then
+        count=`expr $count + 1`
+    fi
 fi
 if [ $count -gt 3 ]
 then
@@ -154,7 +175,7 @@ fi
 echo ""
 echo ""
 echo ""
-echo "Disk usage info:"
+echo "Disk usage info"
 echo "================================"
 df -h | grep "Avail"; df -h | grep "/$"
 echo "================================"
