c1eb5bfaf2768872cbc9f741eae4298d5cae404e
Alan_Yoon <shwan.yoon@hotmail.com>
2022-07-11 00:25:27 +0900

    Update cli_checker_validator.sh

--- cli_checker_validator.sh
+++ cli_checker_validator.sh
@@ -180,3 +180,22 @@ echo "================================"
 df -h | grep "Avail"; df -h | grep "/$"
 echo "================================"
 echo ""
+echo "Node uptime"
+echo "================================"
+pro_nm="aptos-node"
+
+pro_sday=`ps -eo lstart,pid,cmd|grep -w "$pro_nm" |grep -v "grep" |
+awk '{ 
+       cmd="date -d\""$1 FS $2 FS $3 FS $4 FS $5"\" +\047%Y-%m-%d %H:%M:%S\047"; 
+       cmd | getline d; close(cmd); $1=$2=$3=$4=$5=""; printf "%s\n",d$0 }' 2>/dev/null |
+       awk '{print $1}'`
+pro_cday=`date +%Y-%m-%d`
+pro_sday_sec=`date -d $pro_sday +%s`
+pro_cday_sec=`date -d $pro_cday +%s`
+day_sec_diff=`echo "($pro_cday_sec - $pro_sday_sec) / 86400" |bc`
+
+echo " Process $pro_nm has been running for" $day_sec_diff "days"
+
+echo "================================"
+echo ""
+
