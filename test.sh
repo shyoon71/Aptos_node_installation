917110680ff77cc28465e8399e3a22247ae6712a
Alan_Yoon <shwan.yoon@hotmail.com>
2022-07-10 20:45:11 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ count=0
 sync=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "aptos_state_sync_version")
 sync=$(echo "$sync"|sed -n -e '5p')
 sync3=$(echo $sync | grep -o '[0-9]*')
-echo "\e[1m\e[33mSyncing Progress \e[0m"
+echo -e "\e[1m\e[33mSyncing Progress \e[0m"
 echo "================================"
 echo "$sync"
 sleep 2
@@ -25,7 +25,7 @@ echo "$epoch"
 echo "================================"
 if [ $sync4 -gt $sync3 ]
 then
-    echo "\e[1m\e[33mok. \e[0m"
+    echo -e "\e[1m\e[33mok. \e[0m"
     count=`expr $count + 1`
 else
     echo ">>>> Not ok!! <<<<"
@@ -34,7 +34,7 @@ echo ""
 highest=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "highest")
 highest=$(echo "$highest"|sed -n -e '5p')
 highest2=$(echo $highest | grep -o '[0-9]*')
-echo "\e[1m\e[33mSyncing Speed \e[0m"
+echo -e "\e[1m\e[33mSyncing Speed \e[0m"
 echo "================================"
 echo "$highest"
 sleep 2
@@ -45,7 +45,7 @@ echo "$sync2"
 echo "================================"
 if [ $sync4 -gt $highest2 ]
 then
-    echo "\e[1m\e[33mok.No lag. \e[0m"
+    echo -e "\e[1m\e[33mok.No lag. \e[0m"
 else
     if [ $highest2 -gt $sync4 ]
     then
@@ -58,7 +58,7 @@ in=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "inbound")
 in=$(echo "$in"|sed -n -e '1p')
 in3=$(echo $in | grep -o '[0-9]*')
 export in4=$(echo "${in3:(-2)}")
-echo "\e[1m\e[33mInbound Connection \e[0m"
+echo -e "\e[1m\e[33mInbound Connection \e[0m"
 echo "================================"
 echo "$in"
 echo "================================"
@@ -70,7 +70,7 @@ else
     then
         echo ">>>> Not ok!! <<<< if you are already in AIT period now."
     else
-        echo "\e[1m\e[33mok. \e[0m"
+        echo -e "\e[1m\e[33mok. \e[0m"
         count=`expr $count + 1`
     fi
 fi
@@ -79,7 +79,7 @@ out=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "outbound")
 out=$(echo "$out"|sed -n -e '1p')
 out3=$(echo $out | grep -o '[0-9]*')
 export out4=$(echo "${out3:(-2)}")
-echo "\e[1m\e[33mOutbound Connection \e[0m"
+echo -e "\e[1m\e[33mOutbound Connection \e[0m"
 echo "================================"
 echo "$out"
 echo "================================"
@@ -89,16 +89,16 @@ then
 else
     if [ $out4 -eq 0 ]
     then
-        echo "\e[1m\e[33mValidators don't need to outbound. No problem. \e[0m"
+        echo -e "\e[1m\e[33mValidators don't need to outbound. No problem. \e[0m"
     else
-        echo "\e[1m\e[33mok. \e[0m"
+        echo -e "\e[1m\e[33mok. \e[0m"
     fi
 fi
 echo ""
 v1=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "aptos_consensus_vote_nil_count")
 v1=$(echo "$v1"|sed -n -e '3p')
 v3=$(echo $v1 | grep -o '[0-9]*')
-echo "\e[1m\e[33mVoting Progress \e[0m"
+echo -e "\e[1m\e[33mVoting Progress \e[0m"
 echo "================================"
 echo "$v1"
 sleep 3
@@ -109,7 +109,7 @@ echo "$v5"
 echo "================================"
 if [ $v6 -gt $v3 ]
 then
-    echo "\e[1m\e[33mok. \e[0m"
+    echo -e "\e[1m\e[33mok. \e[0m"
     count=`expr $count + 1`
 else
     echo ">>>> Not ok!! <<<<"
@@ -119,7 +119,7 @@ sleep 2
 r1=$(curl 127.0.0.1:9101/metrics 2> /dev/null | grep "aptos_consensus_current_round")
 r1=$(echo "$r1"|sed -n -e '3p')
 r3=$(echo $r1 | grep -o '[0-9]*')
-echo "\e[1m\e[33mConsensus Round Progess \e[0m"
+echo -e "\e[1m\e[33mConsensus Round Progess \e[0m"
 echo "================================"
 echo "$r1"
 sleep 2
@@ -130,16 +130,16 @@ echo "$r5"
 echo "================================"
 if [ $r6 -gt $r3 ]
 then
-    echo "\e[1m\e[33mok. \e[0m"
+    echo -e "\e[1m\e[33mok. \e[0m"
     count=`expr $count + 1`
 else
     echo ">>>> Not ok!! <<<<"
 fi
 echo ""
 v7=`echo "scale=2;$v6*100/$r6"|bc`
-echo "\e[1m\e[33mVoting Success Ratio \e[0m"
+echo -e "\e[1m\e[33mVoting Success Ratio \e[0m"
 echo "================================"
-echo 'rate_now : \e[1m\e[33m'$v7'%\e[0m  should be >=25% at the end of the test period.'
+echo -e 'rate_now : \e[1m\e[33m'$v7'%\e[0m  should be >=25% at the end of the test period.'
 echo "================================"
 echo ""
 if [[ `echo "$v7 > 60" | bc` -eq 1 ]]
@@ -150,9 +150,9 @@ if [ $count -gt 3 ]
 then
     if [ $count -gt 4 ]
     then
-        echo "\e[1m\e[33mDone! Check result's so amazing! \e[0m"
+        echo -e "\e[1m\e[33mDone! Check result's so amazing! \e[0m"
     else
-        echo "\e[1m\e[33mDone! Check result's good. \e[0m"
+        echo -e "\e[1m\e[33mDone! Check result's good. \e[0m"
     fi
 else
     echo "Done. You should check carefully at the parts that are "Not ok!!" now."
