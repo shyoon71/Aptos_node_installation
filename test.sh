2971b5394ebe318407cc011ab63f2ba0ff4646a9
Alan_Yoon <shwan.yoon@hotmail.com>
2022-07-11 01:22:58 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -195,6 +195,5 @@ pro_cday_sec=`date -d $pro_cday +%s`
 day_sec_diff=`echo "($pro_cday_sec - $pro_sday_sec) / 86400" |bc`
 echo "Process $pro_nm has been running for" $day_sec_diff "days."
 ps -eo pid,etime,cmd | grep aptos-node | grep -v grep
-ps -eo etime,command | grep aptos-node | grep -v grep | awk '{print $1}'
 echo "================================"
 echo ""
