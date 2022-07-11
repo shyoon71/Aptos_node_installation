d7c32e840c4c1483b31686c1571e2b6dbcf62b03
Alan_Yoon <shwan.yoon@hotmail.com>
2022-07-11 10:33:41 +0900

    Update tempCodeRunnerFile.sh

--- tempCodeRunnerFile.sh
+++ tempCodeRunnerFile.sh
@@ -1,1 +1,7 @@
-echo -e "\e[1m\e[33mSearching your info now... \e[0m"
+up=$(pgrep -f aptos-node)
+up2=$(ps -p $up -o etime)
+up3=$(echo "$up2"|sed -n -e '2p')
+day=$(echo $up3 | cut -d '-' -f1)
+hour=$(echo $up3 | cut -d ':' -f2)
+min=$(echo $up3 | cut -d ':' -f3)
+echo "Node has been running for " $day "days" $hour "hours" $min "minutes"
