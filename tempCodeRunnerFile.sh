4309842c7ecb7433604a58c3f33493cd44886a18
Alan_Yoon <shwan.yoon@hotmail.com>
2022-07-11 10:41:22 +0900

    Update tempCodeRunnerFile.sh

--- tempCodeRunnerFile.sh
+++ tempCodeRunnerFile.sh
@@ -1,7 +1,13 @@
+echo "Disk Usage Info"
+echo "================================"
+df -h | grep "Avail"; df -h | grep "/$"
+echo "================================"
+echo ""
+echo "Node Uptime"
+echo "================================"
+pid=$(ps -ef|grep aptos-node)
+echo "$pid"|sed -n -e '1p'
 up=$(pgrep -f aptos-node)
-up2=$(ps -p $up -o etime)
-up3=$(echo "$up2"|sed -n -e '2p')
-day=$(echo $up3 | cut -d '-' -f1)
-hour=$(echo $up3 | cut -d ':' -f2)
-min=$(echo $up3 | cut -d ':' -f3)
-echo "Node has been running for " $day "days" $hour "hours" $min "minutes"
+ps -p $up -o etime
+echo "================================"
+echo ""
