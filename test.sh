87dd319c869b13ff95faaffc49b4106691e9959c
Alan_Yoon <shwan.yoon@hotmail.com>
2022-07-12 14:20:23 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -19,7 +19,7 @@ sleep 1
 echo "Checking Liveness"
 echo "================================"
 curl 127.0.0.1:80 2> /dev/null; curl 127.0.0.1:8080 2> /dev/null
-live=$(curl 127.0.0.1:80) ; live2=$(curl 127.0.0.1:8080)
+live=$(echo -e "curl 127.0.0.1:80 \n") 2> /dev/null; live2=$(echo -e "curl 127.0.0.1:8080 \n") 2> /dev/null
 echo "================================"
 if [ -z $live ]
 then
