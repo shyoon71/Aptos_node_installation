654a93fa47021d817964891e01090e779bc5d98d
Alan_Yoon <shwan.yoon@hotmail.com>
2022-07-12 14:27:57 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -19,7 +19,8 @@ sleep 1
 echo "Checking Liveness"
 echo "================================"
 curl 127.0.0.1:80 2> /dev/null; curl 127.0.0.1:8080 2> /dev/null
-live=$(echo -e "curl 127.0.0.1:80 \n") > /dev/null; live2=$(echo -e "curl 127.0.0.1:8080 \n") > /dev/null
+live=$(curl 127.0.0.1:80) > /dev/null; live2=$(curl 127.0.0.1:8080) > /dev/null
+\n
 echo "================================"
 live=$(echo $live | grep -o '[0-9]*')
 live2=$(echo $live2 | grep -o '[0-9]*')
