52eadc0f12f59aec66289418174bbcef2a296d4e
Alan_Yoon <shwan.yoon@hotmail.com>
2022-07-14 04:30:10 +0900

    Update tempCodeRunnerFile.sh

--- tempCodeRunnerFile.sh
+++ tempCodeRunnerFile.sh
@@ -1,17 +1,2 @@
-echo "Checking Liveness"
+echo "Voting Progress        checking 30s diff." 
 echo "================================"
-curl 127.0.0.1:80 2> /dev/null; curl 127.0.0.1:8080 2> /dev/null
-live=$(curl 127.0.0.1:80) ; live2=$(curl 127.0.0.1:8080)
-echo "================================"
-if [ -z $live ]
-then
-    if [ -z $live2 ]
-    then
-        echo "Can't fetch out your connection status."
-        echo "Node looks like have no liveness"
-    else
-        echo "ok."    
-    fi
-else
-    echo "ok."
-fi
